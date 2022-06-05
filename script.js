bb2a80ee8d31f1cce2fe6a80590e12467fd5c83d
Lucas Gabriel Fregulia Bautitz <72616457+Bauditz@users.noreply.github.com>
2022-06-05 12:53:26 -0300

    Add files via upload
    A second exercise to work with modal windows. Training function, eventListener, document and more!

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// CREATE VAR WITH FUNCTIONS TO MAKE THE CODE MORE CLEANER! \/
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// CREATE VAR WITH FUNCTIONS TO MAKE THE CODE MORE CLEANER! \/
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// CREATE A FUNCTION TO MAKE A KEY TO CLOSE THE MODAL \/
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
